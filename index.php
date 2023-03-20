cdf8247da0a3755a80de7cd8834693c0cd7f04fc
timxx21 <112845568+timxx21@users.noreply.github.com>
2023-03-20 09:10:27 +0300

    Update index.php

--- index.php
+++ index.php
@@ -2,67 +2,66 @@
 // Отправляем браузеру правильную кодировку,
 // файл index.php должен быть в кодировке UTF-8 без BOM.
 header('Content-Type: text/html; charset=UTF-8');
-
+echo "<link rel='stylesheet' href='style.css'>";
 // В суперглобальном массиве $_SERVER PHP сохраняет некторые заголовки запроса HTTP
 // и другие сведения о клиненте и сервере, например метод текущего запроса $_SERVER['REQUEST_METHOD'].
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-  // В суперглобальном массиве $_GET PHP хранит все параметры, переданные в текущем запросе через URL.
-  if (!empty($_GET['save'])) {
-    // Если есть параметр save, то выводим сообщение пользователю.
-    print('Спасибо, результаты сохранены.');
-  }
-  // Включаем содержимое файла form.php.
-  include('form.php');
-  // Завершаем работу скрипта.
-  exit();
+// В суперглобальном массиве $_GET PHP хранит все параметры, переданные в текущем запросе через URL.
+if (!empty($_GET['save'])) {
+// Если есть параметр save, то выводим сообщение пользователю.
+print('Спасибо, результаты сохранены.');
+}
+// Включаем содержимое файла form.php.
+include('form.php');
+// Завершаем работу скрипта.
+exit();
 }
 // Иначе, если запрос был методом POST, т.е. нужно проверить данные и сохранить их в XML-файл.
 
 // Проверяем ошибки.
 $errors = FALSE;
 if (empty($_POST['fio'])) {
-  print('Заполните имя.<br/>');
-  $errors = TRUE;
+print('Заполните имя.<br/>');
+$errors = TRUE;
 }
 
 if (empty($_POST['year']) || !is_numeric($_POST['year']) || !preg_match('/^\d+$/', $_POST['year'])) {
-  print('Заполните год.<br/>');
-  $errors = TRUE;
+print('Заполните год.<br/>');
+$errors = TRUE;
 }
 
-
-// *************
-// Тут необходимо проверить правильность заполнения всех остальных полей.
-// *************
+if (empty($_POST['email'])) {
+print('Заполните email.<br/>');
+$errors = TRUE;
+}
 
 if ($errors) {
-  // При наличии ошибок завершаем работу скрипта.
-  exit();
+// При наличии ошибок завершаем работу скрипта.
+exit();
 }
 
 // Сохранение в базу данных.
 
-$user = 'db'; // Заменить на ваш логин uXXXXX
-$pass = '123'; // Заменить на пароль, такой же, как от SSH
-$db = new PDO('mysql:host=localhost;dbname=test', $user, $pass,
-  [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]); // Заменить test на имя БД, совпадает с логином uXXXXX
+$user = 'u52808';
+$pass = '9337244';
+$db = new PDO('mysql:host=localhost;dbname=u52808', $user, $pass, [PDO::ATTR_PERSISTENT => true]);
 
 // Подготовленный запрос. Не именованные метки.
 try {
-  $stmt = $db->prepare("INSERT INTO application SET name = ?");
-  $stmt->execute([$_POST['fio']]);
+$stmt = $db->prepare("INSERT INTO form SET name = ?, year = ?, email = ?");
+$stmt -> execute([$_POST['fio'], $_POST['year'], $_POST['email']]);
 }
 catch(PDOException $e){
-  print('Error : ' . $e->getMessage());
-  exit();
+print('Error : ' . $e->getMessage());
+exit();
 }
 
-//  stmt - это "дескриптор состояния".
- 
-//  Именованные метки.
+// stmt - это "дескриптор состояния".
+
+// Именованные метки.
 //$stmt = $db->prepare("INSERT INTO test (label,color) VALUES (:label,:color)");
 //$stmt -> execute(['label'=>'perfect', 'color'=>'green']);
- 
+
 //Еще вариант
 /*$stmt = $db->prepare("INSERT INTO users (firstname, lastname, email) VALUES (:firstname, :lastname, :email)");
 $stmt->bindParam(':firstname', $firstname);
